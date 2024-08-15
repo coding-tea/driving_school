677892435f89f6d0e82f62bc49b9d4eaa13bcf8f
coding-tea <prgaming.ismail@gmail.com>
2024-08-15 15:22:59 +0100

    add office lang

---
+++ lang/ar/office.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    // pages
+    'page_index' => [
+        'page_title' => 'Creer un Office',
+        'page_dt_action_delete_all' => 'Supprimer les Offices sélectionnées',
+        'page_th_item' => 'Office',
+    ],
+
+    'page_create' => [
+        'page_title' => 'Liste des Offices',
+    ],
+
+    'page_edit' => [
+        'page_title' => 'Modifier le Office',
+        'page_title_with_item' => 'Liste des Office',
+    ],
+
+    // labels
+    'name' => 'Nom',
+    'name_ar' => 'Nom (Arabe)',
+    'cin' => "Numéro de la carte identité",
+    'address' => 'Adresse',
+    'birthday' => 'Date de naissance',
+    'birth_city' => 'Ville de naissance',
+    'reference' => 'Référence',
+    'signin_date' => "Date de inscription",
+    'image' => 'Image',
+    'cinimage' => "Image de la carte identité",
+
+    // alerts
+    'created_notification' => 'Office créé avec succès',
+    'updated_notification' => 'Le Office a été mise à jour avec succès',
+    'deleted_notification' => 'Office supprimée avec succès',
+    'selected_deleted_notification' => 'les Offices sélectionnées supprimées avec succès',
+
+];
